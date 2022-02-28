0bb6c97dcf67bbb4a15cfbcc985630acbde88e77
azf20 <azfuller20@gmail.com>
2022-02-28 15:26:03 +0000

    Remove event-based approach

--- packages/hardhat/contracts/EpochOracle.sol
+++ packages/hardhat/contracts/EpochOracle.sol
@@ -5,25 +5,9 @@ pragma solidity >=0.8.0 <0.9.0;
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract EpochOracle is Ownable {
-    event NewEpochBlock(uint256 epoch, uint256 networkId, bytes32 blockHash);
-
-    struct EpochBlockUpdate {
-        // EIP-155 (chainid) https://github.com/ethereum/EIPs/blob/master/EIPS/eip-155.md
-        uint16 networkId;
-        bytes32 blockHash;
-    }
 
     constructor(address _owner) {
         transferOwnership(_owner);
-    }
-
-    /// @notice Set multiple epoch blocks
-    /// @dev Emits events with the updates
-    function setEpochBlocks(uint256 _epoch, EpochBlockUpdate[] calldata _updates) external {
-        for (uint256 i = 0; i < _updates.length; i++) {
-            uint256 networkId = _updates[i].networkId;
-            emit NewEpochBlock(_epoch, networkId, _updates[i].blockHash);
-        }
     }
 
     /// @notice Set multiple epoch blocks
